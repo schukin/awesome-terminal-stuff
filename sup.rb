feaaa50dd619975dde399ea093a3d8f8c1db0dcd
Dave Schukin <daveschukin@gmail.com>
2014-02-04 01:22:37 -0800

    I CAN HAZ COMMENTZ.

---
+++ sup.rb
@@ -0,0 +1,15 @@
+random_facts = ['You look fucking awesome today', 
+	'Did you just get a haircut?',
+	'You are awesome',
+	'Teach me how to dougie']
+
+random_fact = random_facts.sample
+random_fact.upcase!
+
+puts "****************************
+* HELLO DAVE               
+*                          
+* WELCOME TO YOUR COMPUTER 
+*                          
+* #{random_fact}
+****************************"
