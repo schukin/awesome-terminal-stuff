67fc76fd539cd477e353bcff4f6b5cc837ca23ba
Dave Schukin <daveschukin@gmail.com>
2014-02-04 23:07:33 -0800

    Cleaned shit up

--- sup.rb
+++ sup.rb
@@ -1,3 +1,20 @@
+def box_text(some_text, box_char)
+	some_text_lines = some_text.split(/\r?\n/)
+	line_lengths = some_text_lines.collect { |line| line.length }
+	line_length = line_lengths.max
+
+	# Wrap each line in box_char
+	some_text_lines = some_text_lines.collect { |line| "#{box_char} #{line.ljust(line_length)} #{box_char}" }
+
+	box_char_line = box_char.ljust(line_length + 4, box_char)
+
+	# Add box_char_line to beginning & end
+	some_text_lines.unshift(box_char_line)
+	some_text_lines.push(box_char_line)
+
+	some_text_lines
+end
+
 random_facts = ['You look fucking awesome today', 
 	'Did you just get a haircut?',
 	'You are awesome',
@@ -12,25 +29,4 @@ WELCOME TO YOUR COMPUTER
 
 #{random_fact}"
 
-welcome_text_lines = welcome_text.split(/\r?\n/)
-line_lengths = welcome_text_lines.collect { |line| line.length }
-line_length = line_lengths.max
-
-# Wrap each line in '*'
-welcome_text_lines = welcome_text_lines.collect { |line| "* #{line.ljust(line_length)} *" }
-
-starline = '*'.ljust(line_length + 4, '*')
-
-# Add starline to beginning & end
-welcome_text_lines.unshift(starline)
-welcome_text_lines.push(starline)
-
-puts welcome_text_lines
-
-# puts "****************************
-# * HELLO DAVE               
-# *                          
-# * WELCOME TO YOUR COMPUTER 
-# *                          
-# * #{random_fact}
-# ****************************"
+puts box_text(welcome_text, '*')
