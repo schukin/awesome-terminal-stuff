47b5a0fcbef9ed7331a0a0494f8abb5571806010
Dave Schukin <daveschukin@gmail.com>
2014-02-05 12:56:35 -0800

    More facts.

--- sup.rb
+++ sup.rb
@@ -2,8 +2,14 @@ require_relative 'lib/ext/string.rb'
 
 random_fact = ['You look fucking awesome today', 
 				'Did you just get a haircut?',
-				'You are awesome',
-				'Teach me how to dougie'].sample.upcase
+				'FYI you are awesome',
+				"BTW You're Funny. Like, LOL style.",
+				"Today's outfit = Thumbs up",
+				"You have the power to start and *win* a dance-off",
+				"Have you been working out?",
+				"FYI, all your friends worry they aren't as funny as you.",
+				"You pick the best radio stations when you're riding shotgun.",
+				"Your sneezes sound like a chorus of angels giggling."].sample.upcase
 
 welcome_text = "HELLO DAVE
 
